dd61dc5ee687a8336b1bcf76bc7bac04a85d4403
orcax7 <126786165+orcax7@users.noreply.github.com>
2023-12-25 11:26:55 +0700

    Update app.js first
    Update app.js first

--- src/App.js
+++ src/App.js
@@ -3,13 +3,9 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  const divStyle={
-    color:'red',
-    backgroundColor:'powderblue',
-    fontSize:'larger',
-    padding:'16px'
-  }
-  return <div style={divStyle}>Hello React</div>
+  return (
+    <h3>Hello wWrld <br/>สวัสดีชาวโลก</h3>
+  );
 }
 
 export default App;
