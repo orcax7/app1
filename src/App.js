94437ddf71fc984234c6bfc73be263b231143967
orcax7 <126786165+orcax7@users.noreply.github.com>
2024-01-19 14:21:47 +0700

    Refsfunc
    Refsfunc

--- src/App.js
+++ src/App.js
@@ -1,23 +1,7 @@
-import React from 'react';
 import logo from './logo.svg';
-// import './App.css';
-// import './style.css';
-import { Header } from './func-components';
-import { Content } from './func-components';
-import { Footer } from './func-components';
-import Calender from './class-components';
+import React from 'react';
+import RefsClass from './refs-class'
 
-function App() {
-  return (
-    <>
-    <Header/>
-    <p><center><Calender/></center></p>
-    <Content/>
-    <Footer/>
-  </> 
-
-  )
+export default function App(){
+  return <Refsfunc/>
 }
-
-
-export default App;
