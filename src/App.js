0737bc6c4b3f9911756eb74037fafe0c6c073bf4
orcax7 <126786165+orcax7@users.noreply.github.com>
2024-02-19 10:48:48 +0700

    form-text
    form-text

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from "react"
-import FormSearch from "./form-search"
+import FormText from "./form-text"
 
 export default function App() {
-  return <FormSearch/>
+  return <FormText/>
 }
